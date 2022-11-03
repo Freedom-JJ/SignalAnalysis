e8a24ceaef9ab9178a83f6cc1f032cae84a62676
DoYou <1714203542@qq.com>
2022-11-03 18:31:10 +0800

    修复某些UUID文件重复问题

--- src/signA/Utils/UUIDUtil.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "UUIDUtil.h"
-
-QString UUIDUtil::GetUUID(){
-    QUuid uuid = QUuid::createUuid();
-//    uuidstr = uuid.toString();
-    QString str = uuid.toString();
-    return str.mid(1,str.size()-2);
-}
