711e6c29a14da664222c4d7a23b11d2dd07efa68
longrookie <rookiemez@163.com>
2022-11-10 23:38:15 +0800

    测试dictionaryController的删除、添加、更新接口，测试代码在tDemo文件夹下

--- src/signA/tDemo/dictionarycontrollertest.cpp
+++ src/signA/tDemo/dictionarycontrollertest.cpp
@@ -22,3 +22,56 @@ void testDictionary()
         qInfo()<<mstoqs(dict->getDictValue());
     }
 }
+
+
+
+void testDictionary_add()
+{
+    DictionaryController dictionaryController = DictionaryController();
+    //添加Dictionary
+    Dictionary* dictionary = new Dictionary();
+    dictionary->setDictName("hel");
+    dictionary->setDictValue("gegege");
+    long long dictId = 0;
+    dictionaryController.addDictionary(dictionary,dictId);
+    qInfo()<<"添加字典记录，新添加的字典id为"<<dictId;
+}
+
+
+
+
+
+
+
+void testDictionary_delete()
+{
+    DictionaryController dictionaryController = DictionaryController();
+
+    //添加Dictionary
+    Dictionary* dictionary = new Dictionary();
+    dictionary->setDictName("hel");
+    dictionary->setDictValue("gegege");
+    long long dictId = 0;
+    dictionaryController.addDictionary(dictionary,dictId);
+
+    //删除新添加的记录
+    Result result = dictionaryController.deleteDictionaryByDictId(dictId);
+    qInfo()<<mstoqs(result.getMsg());
+}
+
+void testDictionary_update()
+{
+    DictionaryController dictionaryController = DictionaryController();
+    //添加Dictionary
+    Dictionary* dictionary = new Dictionary();
+    dictionary->setDictName("hel");
+    dictionary->setDictValue("gegege");
+    long long dictId = 0;
+    dictionaryController.addDictionary(dictionary,dictId);
+    dictionary->setId(dictId);
+
+    //更新Dictionary,更新Dictionary时需要保证其id已经赋值，因为是根据主键id更新
+    dictionary->setDictValue("new_ge");
+    Result result = dictionaryController.updateDictionary(dictionary);
+    qInfo()<<mstoqs(result.getMsg());
+}
--- src/signA/tDemo/dictionarycontrollertest.h
+++ src/signA/tDemo/dictionarycontrollertest.h
@@ -3,7 +3,18 @@
 
 #include "../Controller/dictionarycontroller.h"
 
+//测试查询接口
 void testDictionary();
+
+//测试添加、删除和更新接口
+void testDictionary_add();
+
+
+//测试删除接口
+void testDictionary_delete();
+
+//测试更新接口
+void testDictionary_update();
 
 
 #endif // DICTIONARYCONTROLLERTEST_H
